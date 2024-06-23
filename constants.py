f0ea5dc84ce911970f4a729bf77e3deb3bde17b0
Deelite34 <benegeserit@o2.pl>
2024-06-23 18:37:46 +0200

    Integrate migrations with alembic

--- constants.py
+++ constants.py
@@ -4,6 +4,10 @@ import discord
 
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
 TEST_GUILD: Final[str] = discord.Object(id=int(os.getenv("TEST_GUILD")))
+DB_USERNAME = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+
 DATABASE_URL: Final[str] = (
-    f"postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@db/{os.getenv("POSTGRES_DB")}"
+    f"postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@db/{DB_NAME}"
 )
